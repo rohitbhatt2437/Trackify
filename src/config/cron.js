26300bc6510d679a93a621d64811eb50a493adbd
Rohit Bhatt <bhattrohed@gmail.com>
2025-09-02 19:39:17 +0530

    cron.js added

--- src/config/cron.js
+++ src/config/cron.js
@@ -1,12 +1,13 @@
-import cron from "cron"
-import https from "https"
+import cron from "cron";
+import https from "https";
 
-const job = new cron.CronJob("*/14****", function(){
-    https.get(process.env.API_URL, (res)=>{
-        if(res.statusCode===200) console.log("Get request sent successfully")
-        else console.log("Get request failed", res.statusCode);
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get(process.env.API_URL, (res) => {
+      if (res.statusCode === 200) console.log("GET request sent successfully");
+      else console.log("GET request failed", res.statusCode);
     })
-    .on("error", (e)=>console.error("Error while sending request", e))
-})
+    .on("error", (e) => console.error("Error while sending request", e));
+});
 
 export default job;
